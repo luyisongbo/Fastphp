85aa6124cb2497f4602381b1f969342c8f33fe87
luyi <1071028547@qq.com>
2018-01-29 17:35:46 +0800

    base/controller

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -2,7 +2,7 @@
 namespace fastphp;
 
 //框架根目录
-define('CORE_PATH') or define('CORE_PATH',__DIR__);
+defined('CORE_PATH') or define('CORE_PATH',__DIR__);
 
 //Fastphp框架核心
 class Fastphp{
@@ -17,6 +17,7 @@ class Fastphp{
     //运行程序
     public function run(){
         //var_dump($this->config);
+        spl_autoload_register(array($this, 'loadClass'));
         $this->route();
     }
 
@@ -67,13 +68,31 @@ class Fastphp{
 
     //自动加载类
     public function loadClass($className){
+        $classMap = $this->classMap();
 
+        if(isset($classMap[$className])){
+            //包含内核文件
+            $file = $classMap[$className];
+        }elseif(strpos($className, '\\') !== false){
+            //包含app文件
+            $file = APP_PATH . str_replace('\\', '/', $className) . 'php';
+            if(!is_file($file)){
+                return;
+            }
+        }else{
+            return;
+        }
+        include $file;
     }
 
     //内核文件命名空间映射关系
     protected function classMap(){
         return[
             'fast\base\Controller' => CORE_PATH . '/base/Controller.php',
+            'fast\base\Model' => CORE_PATH . '/base/Model.php',
+            'fast\base\View' => CORE_PATH . 'base/View.php',
+            'fast\db\Db' => CORE_PATH . 'db/Db.php',
+            'fast\db\Sql.php' => CORE_PATH . 'db/Sql.php',
         ];
     }
 
---
+++ fastphp/base/Controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace fastphp\base;
+
+class Controller{
+
+    protected $_controller;
+    protected $_action;
+    protected $_view;
+}
