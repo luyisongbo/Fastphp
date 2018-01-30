0133e66335eedc2899bc2c7dc2c6355a6c29d3ce
luyi <1071028547@qq.com>
2018-01-30 17:38:23 +0800

    new

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,3 +1,13 @@
 <?php
 namespace app\controllers;
+use fastphp\base\Controller;
+use app\models\ItemModel;
 
+class IndexController extends Controller
+{
+// 首页方法，测试框架自定义DB查询
+    public function index()
+    {
+       echo 'success!';
+    }
+}
--- fastphp/base/Controller.php
+++ fastphp/base/Controller.php
@@ -6,4 +6,18 @@ class Controller{
     protected $_controller;
     protected $_action;
     protected $_view;
+
+    public function __construct($controller, $action){
+        $this->_controller = $controller;
+        $this->_action = $action;
+        $this->_view = new View($controller, $action);
+    }
+
+    public function assign($name, $value){
+        $this->_view->assgin($name, $value);
+    }
+
+    public function render(){
+        $this->_view->render();
+    }
 }
